8a13502594d75b2ba5173dee82ed73f429ba23fc
paulo <paulo.m.oliveira022@gmail.com>
2022-09-16 15:42:27 -0300

    Extraindo Classe - Refatoração

--- livraria_online/src/br/com/caelum/livraria/service/SelecaoDeLivro.java
+++ livraria_online/src/br/com/caelum/livraria/service/SelecaoDeLivro.java
@@ -9,6 +9,7 @@ import org.javamoney.moneta.Money;
 
 import br.com.caelum.livraria.dominio.CalculadoraFrete;
 import br.com.caelum.livraria.dominio.CarrinhoDeCompras;
+import br.com.caelum.livraria.dominio.CarrinhoDeComprasFactory;
 import br.com.caelum.livraria.dominio.Cliente;
 import br.com.caelum.livraria.dominio.ISBN;
 import br.com.caelum.livraria.dominio.Livro;
@@ -19,26 +20,20 @@ public class SelecaoDeLivro {
 	private final TodosLivros todosLivros;
 	private final CalculadoraFrete calculadoraFrete;
 	private final Set<CarrinhoDeCompras> carrinhos;
-	
+	private final CarrinhoDeComprasFactory carrinhoFactory;
 	
 	public SelecaoDeLivro(TodosLivros todosLivros, CalculadoraFrete calculadoraFrete) {
 		this.todosLivros = todosLivros;
 		this.calculadoraFrete = calculadoraFrete;
 		this.carrinhos = new HashSet<>();
+		this.carrinhoFactory = new CarrinhoDeComprasFactory();
 	}
 
-	public CarrinhoDeCompras adicionarLivroNoCarrinhoDoCliente(ISBN isbn, Cliente cliente, String cep) {
+	public CarrinhoDeCompras adicionarLivroNoCarrinhoDoCliente(ISBN isbn, Cliente cliente) {
 		Livro livro = todosLivros.por(isbn);
-		Money valorFrete = calculadoraFrete.baseadoEm(cep);
+		Money valorFrete = calculadoraFrete.baseadoEm(cliente.getCep() );
 		
-		CarrinhoDeCompras carrinho = new CarrinhoDeCompras(cliente, livro, valorFrete, now());
-		if(carrinhos.contains(carrinho)) {
-			carrinho = carrinhos.stream()
-					.filter(umCarrinho -> umCarrinho.doCliente(cliente))
-					.findFirst().orElse(null);
-			if(carrinho != null)carrinho.getLivros().adicionar(livro);
-		}
-		else carrinhos.add(carrinho);
+		CarrinhoDeCompras carrinho = carrinhoFactory.adicionarCarrinho(cliente, livro, valorFrete);
 		return carrinho;
 	}
 }
